32ba561b6641a16b0c1b241f78fedc9404be6975
Robert <robert.m93a@gmail.com>
2020-07-17 15:40:51 +0200

    news

---
+++ resources/views/layouts/components/news.blade.php
@@ -0,0 +1,12 @@
+
+@php
+$sectiontitle = $data['headline'];
+
+@endphp
+
+<section class="section news fullscroll">
+<div class="container">
+	@include('blocks.headline')
+	<div class="news__wrapper" >
+	</div>
+</section>
